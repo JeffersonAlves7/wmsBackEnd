63eae95e668d516c189a8243744fb1a3f2565d76
JeffersonAlves7 <ilj031420@hotmail.com>
2022-08-15 17:57:59 -0300

    Last changes on 2022-08-15

--- src/app/controller/postView.js
+++ src/app/controller/postView.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 router.get('/postagem', function (req, res) {
     const { usuario, senha } = req.query
-    console.log(req)
     if (usuario !== "pedro" || senha !== "123456") res.status(404).send("Cannot GET " + req.route.path)
 
     res.sendFile(path.join(__dirname, '..', 'view', 'index.html'));
